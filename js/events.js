a597d659dd0818050d9a982e098b4f9f9d0dd590
Kim Foukaris <kimfoukaris@outlook.com>
2017-11-04 08:45:43 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function pressIt(){
 }
 
 function submitIt() {
-  $("form").on("submit", function() {
+  $('input').on("submit", function() {
     alert("Your for is going to be submitted now.")
   })
 }
