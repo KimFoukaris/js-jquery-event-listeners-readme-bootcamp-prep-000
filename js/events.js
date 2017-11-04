1448e7f1e66253fc695fc7147197b80de7b789be
Kim Foukaris <kimfoukaris@outlook.com>
2017-11-04 08:27:01 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,10 +4,15 @@ function getIt() {
     alert ("Hey!")
   })
 }
+$('#typing').on('keydown', function(key) {
+  if (key.which === 71)
+    alert("You pressed G")
+})
+}
 
 $(document).ready(function(){
 
 // call functions here
 getIt()
-
+pressIt()
 });
